4d555453adf3f9e83541643fd1cdc4f6a0f536cd
meikalaheshla <fausto.boshoff@gmail.com>
2022-09-28 01:11:35 +0200

    created admin/home link to post page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,9 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                    <li class="nav-item">
+                                <a class="nav-link @if(Route::is('admin.posts.index')) active @endif" href="{{ route('admin.posts.index')}}">Posts</a>
+                            </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
