d2cd325183b99830c1a17519c3c8df2e529592be
meikalaheshla <fausto.boshoff@gmail.com>
2022-09-27 23:30:38 +0200

    setted route for vue and relative error

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('guest.home');
-});
 
 Auth::routes();
 
 
 Route::middleware('auth')->prefix('admin')->namespace('Admin')->name('admin.')->group(function(){
     Route::get('/', 'HomeController@index')->name('home');
-
+    Route::get('/{any}', function() {
+        abort('404');
+    })->where('any','.*');
 });
+
+
+Route::get('/{any?}', function () {
+    return view('guest.home');
+})->where('any', '.*');
