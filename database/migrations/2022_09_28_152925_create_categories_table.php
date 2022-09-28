017a3bcf5a0ddb8535fd364917bfcb7a6b4458c5
meikalaheshla <fausto.boshoff@gmail.com>
2022-09-28 17:36:03 +0200

    added columns to categories

--- database/migrations/2022_09_28_152925_create_categories_table.php
+++ database/migrations/2022_09_28_152925_create_categories_table.php
@@ -15,6 +15,8 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('label', 20)->unique();
+            $table->string('color', 20)->nullable();
             $table->timestamps();
         });
     }
