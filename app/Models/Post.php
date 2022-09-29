c6a33d5402bacd60762269a610fbed9549bb8a47
meikalaheshla <fausto.boshoff@gmail.com>
2022-09-29 16:58:57 +0200

    created relation via models category->post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+
     protected $fillable = ['title', 'image','content','slug'];
+    public function category()
+    {
+        return $this->belongsTo('App\Models\Category');
+    }
 }
